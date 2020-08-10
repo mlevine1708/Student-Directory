c73df7e799859d61d49a45bfc0a5a15fb109bb90
micah.levine <github email address>
2020-08-09 20:33:06 -0400

    added sessions controller

---
+++ Virtual/app/controllers/sessions_controller.rb
@@ -0,0 +1,46 @@
+class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+    render :login
+  end
+
+  def create
+    @user = User.find_by(username: params[:user][:username])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to students_path
+    else
+      flash[:error] = "Sorry, your username or password was incorrect"
+      redirect_to '/login'
+    end
+  end
+
+  #omniauth login
+  def fbcreate
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.username = auth['info']['name']
+      u.email = auth['info']['email']
+      u.password = auth['uid']   # Secure Random Hex
+    end
+
+    session[:user_id] = @user.id
+
+    redirect_to '/students'
+  end
+
+  def home
+  end
+
+  #logout
+  def destroy
+    session.clear
+    redirect_to '/'
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
+  end
+end
