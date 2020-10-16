74647b1a28fa9f4d87d1466ddc82b1c51f0175ad
micah.levine <github email address>
2020-10-16 16:11:24 -0400

    added search

--- Virtual/app/controllers/teachers_controller.rb
+++ Virtual/app/controllers/teachers_controller.rb
@@ -2,9 +2,13 @@ class TeachersController < ApplicationController
 
   def index
     @teachers = Teacher.all
+    @teachers = Teacher.search(params[:search])
   end
 
   def show
     @teacher = Teacher.find_by(id: params[:id])
   end
+
+
+
 end
--- Virtual/app/models/teacher.rb
+++ Virtual/app/models/teacher.rb
@@ -2,10 +2,14 @@ class Teacher < ApplicationRecord
 
   has_many :students
   has_many :users, through: :students
-  
+
 
   def ordered_students
     students.order_by_grade
+  end
+
+  def self.search(p)
+    Teacher.where("name LIKE ?", "%" + p + "%")
   end
 
 end
